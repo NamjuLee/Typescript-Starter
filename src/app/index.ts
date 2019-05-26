8a1de0567fe248f434d51328214d1a08e87210c6
NJ <nj.namju@gmail.com>
2019-05-26 15:34:54 +0900

    update canvase stater

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,36 @@
-import { My } from './my/my';
+
 
 class App {
-    constructor() {
-        console.log('App Init');
-        const my = new My();
-        console.log(my);
+    ctx: CanvasRenderingContext2D;
+    r: number = 1.0;
+    constructor(id: string) {
+        this.Init(id)
+    }
+    Init(id: string) {
+        const div = document.getElementById(id);
+
+        const canvas = document.createElement('canvas');
+        this.ctx = canvas.getContext('2d');
+
+        div.appendChild(canvas);
+        this.loop();
+
+    }
+    loop(){
+        requestAnimationFrame(() => { this.loop(); });
+        this.ctx.clearRect(0,0, this.ctx.canvas.width, this.ctx.canvas.height);
+        this.Render(this.ctx);
+    }
+    Render(ctx: CanvasRenderingContext2D){
+        const r = Math.sin(this.r);
+        ctx.beginPath();
+        ctx.arc(100 + this.r, 100, 5 + r, 0, 3.14 *2 );
+        ctx.closePath();
+        ctx.fill();
+        console.log('isLoop');
+        this.r += 0.1;
     }
 }
 
-const app = new App();
-console.log('eee');
+const app = new App('main');
+console.log('eee!!');
